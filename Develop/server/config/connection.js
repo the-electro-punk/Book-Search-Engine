700b48a69c56cded4e4f17519d140db190fbbc43
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2022-02-09 20:45:44 -0600

    moved code to a new Repo

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,19 +1,13 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+// const express = require('express');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
 
 // change to apollo and graphql
-
-// const { gql } = require('apollo-server-express')
-
-// const typeDefs = gql``;
-
-// module.exports = typeDefs
-
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,34 +1,42 @@
 const express = require('express');
-const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const authMiddleware = require("./utils/auth")
+
+// const {Resolver, Query} = require('./node_modules/graphql/type/validate.js')
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
-// this connects the server.js file to an apollo server
-const {ApolloServer} = require('apollo-server-express')
-// this collects the typedefs and resolvers files from thew schema folder
-const {typeDefs, resolvers} = require('./schemas')
-const {authMiddleware} = require('./utils/auth')
 
-
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = process.env.PORT || 3005;
-const server = new ApolloServer({
-  typeDefs, 
-  resolvers,
-  context: authMiddleware
-})
-server.applyMiddleware({app})
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize the Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-// app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
+
+// npm run seeds connects to database
